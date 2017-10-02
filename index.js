dca41eb4bacc44385a084e90cc6c6f54fa625371
KyleCurgenvenEDU <31530188+KyleCurgenvenEDU@users.noreply.github.com>
2017-10-02 18:12:26 +1100

    further discord.js testing

--- index.js
+++ index.js
@@ -4,30 +4,69 @@ const Discord = require("discord.js");
 const Token = require('./token.js');
 
 const client = new Discord.Client();
-const guild = new Discord.Guild();
 
 const token = Token.mytoken;
-
 client.on('ready', () => {
    console.log('I am ready!');
-//    console.log(client.users);
-   
-   
+//    console.log();
+    
 });
 client.on('message', message => {
+    //ignore bot messages
+    if(message.author.bot){
+        console.log('Bot typed');
+        return;
+    }
+    //reply with pong
     if(message.content.toLowerCase()==='ping'){
         message.channel.send('pong');
+        // message.reply('pong');
     }
     //experimentation
+    // console.log(client.guilds);
+    console.log(`${message.author.username} sent a message in: ${message.channel.name}`);
+});
 
+// console.log(client.channels);
+// let userName = client.users.get('218553725000744961');
+// console.log(userName);
 
+// guildMemberUpdate working
+client.on('guildMemberUpdate', () =>{
+    console.log(`guild memeber updated`);
 });
 
 
+// client.on('guildMemberAdd', () => {
+//     console.log(`test`);
+// });
 
+client.on('guildMemberAvailable', () => {
+    console.log(`guild member online`);
+});
+
+//logs event when server is updated
+client.on('guildUpdate', guildUpdate =>{
+    console.log('Server updated');
+});
+
+client.login(token);
+
+//=============
+//NOTES
+//=============
 //JSON file with info from last sent message
-//console.log(message.author);
+// console.log(message.author);
 
 //Username and echo content back
 //console.log(message.author.username);
 //console.log(message.author.lastMessage.content);
+// message.content.startsWith('') // startsWith  === String JS
+
+// console.log(Date.now()-message.createdTimestamp);
+
+//search user collection by id for user
+// client.users.get('218553725000744961')
+
+//send DM
+// client.users.get('218553725000744961').sendMessage("test");
